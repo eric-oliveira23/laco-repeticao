4defd01adfcc9a5836ed9b8b8c4f65058326f95b
Eric <ericpoliveira@gmail.com>
2021-11-28 17:50:52 -0300

    laco-repeticao

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laco_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 100;
+
+            while (cont >= 1) 
+            {
+                Console.WriteLine(cont);
+                cont--;
+            }
+            Console.ReadKey();
+
+            cont = 2;
+
+            while (cont <= 100)
+            {
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
